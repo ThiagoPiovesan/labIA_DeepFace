39aa0632e58fd794db5a13fabb18d7fa65293fc0
Thiago Piovesan <thiagoppiovesan@gmail.com>
2021-08-30 10:13:27 -0300

    Versão funcional - separação dos frames

--- deepface_init.py
+++ deepface_init.py
@@ -7,32 +7,35 @@
 # Teste: Versão 1.0
 #-------------------------------------------#
 # Importação de Bibliotecas
+from numpy.lib.npyio import save
 from deepface import DeepFace
 import cv2
 import matplotlib.pyplot as plt
 import json
 import time
 import base64
+import numpy as np
+import datetime
+import pandas as pd
 
 #-------------------------------------------#
 # Captura contínua da câmera
-def loop_and_detect(cam, conf_th):
+def loop_and_detect(cam):
     """Continuously capture images from camera and do object detection.
 
     # Arguments
       cam: the camera instance (video source).
-      conf_th: confidence/score threshold for object detection.
     """
 #-------------------------------------------#
 # Inicialização de Variáveis
     c_timer = 0
-    full_scrn = False
     fps = 0.0
     tic = time.time()
 #-------------------------------------------#
-    # Aux variable
-    last_time = time.time()
+    # Aux timer variable
+    last_time = datetime.datetime.now().second
 #-------------------------------------------#
+    save_frames = []
     # ct = CentroidTracker()
     # auxId = -1
 #-------------------------------------------#
@@ -43,44 +46,43 @@ def loop_and_detect(cam, conf_th):
         # except Exception as e:
         #     print(str(e))
         #     continue
-#-------------------------------------------#
-# Pega 1 frame a cada 30 frames -> 2 frames por segundo
-        # if c_timer == 30:
+#-----------------------------------------------------------------#
         ret, frame = cam.read()
         if frame is None or ret is not True:
-                continue
+            continue
 
-        #     c_timer = 0
-        # else:
-        #     c_timer = c_timer + 1
-        #-------------------------------------------#
-
-        #-------------------------------------------#
+        #-----------------------------------------------------------------#
         try:
-        
-            cv2.imshow('frame',frame)
+            c_timer = datetime.datetime.now().second
+            save_frames.append(frame)               # Salva os frames em um vetor
+            # cv2.imshow('frame',frame)
 
             # if(objectID > auxId):
-        #-------------------------------------------#
+        #-----------------------------------------------------------------#
             # Convert captured image to JPG
             toc = time.time()
-            if (1.0 / (toc - tic)) >= 120:
-
-                ret, buffer = cv2.imencode('.jpg', frame)
+        
+        # Pega os frames acumulados ao longo de 1 segundo
+            if abs(c_timer-last_time) == 1:
+                print(str(abs(c_timer-last_time)))
+                # ret, buffer = cv2.imencode('.jpg', frame)
                 
-                cv2.imwrite('images/teste.jpg', frame) 
+                cv2.imwrite('images/teste.jpg', save_frames[0]) 
 
-                # Convert JPG image to base64
-                # base64_buffer = base64.b64encode(frame)
+                # Convert Frame to base64
+                # base64_buffer = base64.b64encode(buffer)
 
-                captures = DeepFace.analyze('images/teste.jpg', actions=['emotion'])
+                captures = DeepFace.analyze(save_frames[0], actions=['emotion'], enforce_detection=True)
             
-                # print(captures['emotion'])
                 emotion = captures['emotion']
                 print(emotion)
 
-            cv2.imshow('frame', frame) 
-        #-------------------------------------------#   
+                last_time = c_timer
+                save_frames = []
+            #-----------------------------------------------------------------#
+
+            cv2.imshow('Sorria, você está sendo analizado', frame) 
+        #-----------------------------------------------------------------#   
             toc = time.time()
             curr_fps = 1.0 / (toc - tic)
             # calculate an exponentially decaying average of fps number
@@ -104,13 +106,15 @@ def main():
 
     # Connect to camera
     cam = cv2.VideoCapture(0)
+    cv2.namedWindow('Sorria, você está sendo analizado',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Sorria, você está sendo analizado', 640,640)
     # cam.open("rtsp://admin:Pti2389!!@192.168.0.100:554/h264/ch1/sub")
 
     if not cam.isOpened():
         raise SystemExit('ERROR: failed to open camera!')
     
     # Loop to detect
-    loop_and_detect(cam, conf_th=0.3)
+    loop_and_detect(cam)
 
     # Close all
     cam.release()
